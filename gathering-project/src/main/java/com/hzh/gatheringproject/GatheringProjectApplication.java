9dd79ebf0a81919106e7b33e37623728946491f8
nacey5 <1160124552@qq.com>
2022-07-05 11:30:57 +0800

    firstCommit: 基础架构搭设以及配置要求

---
+++ gathering-project/src/main/java/com/hzh/gatheringproject/GatheringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hzh.gatheringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatheringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatheringProjectApplication.class, args);
+    }
+
+}
---
+++ gathering-project/src/main/java/com/hzh/gatheringproject/config/DruidConfig.java
@@ -0,0 +1,22 @@
+package com.hzh.gatheringproject.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author DAHUANG
+ * @date 2022/7/5
+ */
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+}
