449310bd253ff3f7fe6388dbcc1ee15f5175225b
nacey5 <1160124552@qq.com>
2022-09-18 16:29:35 +0800

    seventhCommit: 对数据库的文件进行了添加

--- gathering-project/src/main/java/com/hzh/gatheringproject/GatheringProjectApplication.java
+++ gathering-project/src/main/java/com/hzh/gatheringproject/GatheringProjectApplication.java
@@ -3,6 +3,7 @@ package com.hzh.gatheringproject;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
